7603f54b4235694e8182066a8c6e2cff11a654dd
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-07-25 21:49:53 +0545

    add register functionality

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -88,7 +88,7 @@ const LoginPage = () => {
             </Button>
             <div className="mt-4 text-center text-sm">
               Don't have an account?{" "}
-              <Link to={"/register"} className="underline">
+              <Link to={"/auth/register"} className="underline">
                 Sign up
               </Link>
             </div>
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Button } from "../components/ui/button";
 import {
   Card,
@@ -9,8 +9,40 @@ import {
 } from "../components/ui/card";
 import { Input } from "../components/ui/input";
 import { Label } from "../components/ui/label";
+import { useRef } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { register } from "../http/api";
+import { LoaderCircle } from "lucide-react";
 
 const RegisterPage = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+  const navigate = useNavigate();
+
+  const mutation = useMutation({
+    mutationFn: register,
+    onSuccess: () => {
+      console.log("Login Successful!");
+      navigate("/dashboard/home");
+    },
+  });
+
+  const handleRegisterSubmit = () => {
+    const name = nameRef.current?.value;
+    const email = emailRef.current?.value;
+    const password = passwordRef.current?.value;
+
+    console.log(email, password);
+    //make server call
+    if (!name || !email || !password) {
+      return alert("Please enter email and password");
+    }
+
+    mutation.mutate({ name, email, password });
+  };
+
   return (
     <div className="flex items-center justify-center h-screen">
       <Card className="w-full max-w-sm ">
@@ -18,19 +50,26 @@ const RegisterPage = () => {
           <CardTitle className="text-xl">Sign Up</CardTitle>
           <CardDescription>
             Enter your information to create an account
+            <br />
+            {mutation.isError && (
+              <span className="text-red-500 text-sm">
+                {mutation.error.message}
+              </span>
+            )}
           </CardDescription>
         </CardHeader>
         <CardContent>
           <div className="grid gap-4">
             <div className="grid gap-2">
               <div className="grid gap-2">
-                <Label htmlFor="first-name">Fullname</Label>
-                <Input id="first-name" placeholder="Max" required />
+                <Label htmlFor="name">Name</Label>
+                <Input ref={nameRef} id="name" placeholder="Max" required />
               </div>
             </div>
             <div className="grid gap-2">
               <Label htmlFor="email">Email</Label>
               <Input
+                ref={emailRef}
                 id="email"
                 type="email"
                 placeholder="m@example.com"
@@ -39,15 +78,23 @@ const RegisterPage = () => {
             </div>
             <div className="grid gap-2">
               <Label htmlFor="password">Password</Label>
-              <Input id="password" type="password" />
+              <Input ref={passwordRef} id="password" type="password" />
             </div>
-            <Button type="submit" className="w-full">
-              Create an account
+            <Button
+              onClick={handleRegisterSubmit}
+              className="w-full flex items-center justify-center gap-3"
+              disabled={mutation.isPending}
+            >
+              <span>Create an account</span>
+              {/* Preloader  */}
+              {mutation.isPending && (
+                <LoaderCircle size={16} className="animate-spin" />
+              )}
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
             Already have an account?{" "}
-            <Link to={"/login"} className="underline">
+            <Link to={"/auth/login"} className="underline">
               Sign in
             </Link>
           </div>
