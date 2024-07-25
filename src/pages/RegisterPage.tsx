a7e0c798cfbc41d79f9f1ba7115f49f8dc3a0b55
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-07-25 22:28:44 +0545

    persist token on register

--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -13,8 +13,12 @@ import { useRef } from "react";
 import { useMutation } from "@tanstack/react-query";
 import { register } from "../http/api";
 import { LoaderCircle } from "lucide-react";
+import useTokenStore from "../store";
 
 const RegisterPage = () => {
+
+  const setToken = useTokenStore((state) => state.setToken)
+  
   const nameRef = useRef<HTMLInputElement>(null);
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
@@ -23,8 +27,9 @@ const RegisterPage = () => {
 
   const mutation = useMutation({
     mutationFn: register,
-    onSuccess: () => {
+    onSuccess: (response) => {
       console.log("Login Successful!");
+      setToken(response.data.accessToken);
       navigate("/dashboard/home");
     },
   });
