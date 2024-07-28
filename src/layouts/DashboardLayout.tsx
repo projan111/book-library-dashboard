f2cd86328f0908c11a5b9179ae7d728d14993989
Rojan G. Mul <70033106+projan111@users.noreply.github.com>
2024-07-28 21:27:33 +0545

    tweaking files

--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,5 +1,4 @@
 import {
-  Badge,
   Bell,
   Book,
   Copyright,
@@ -7,9 +6,7 @@ import {
   LogOut,
   Menu,
   Package,
-  Package2,
   Search,
-  ShoppingCart,
   User2,
 } from "lucide-react";
 import { Link, Outlet, Navigate, NavLink } from "react-router-dom";
@@ -48,7 +45,7 @@ const DashboardLayout = () => {
   };
 
   return (
-    <div className=" grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] bg-green-50">
+    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr] bg-green-50">
       <div className="hidden border-r bg-muted/40 md:block">
         <div className="flex h-full max-h-screen flex-col gap-2">
           <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
@@ -83,7 +80,7 @@ const DashboardLayout = () => {
               </NavLink>
             </nav>
           </div>
-          <div className="mt-auto p-4">
+          <div className="mt-auto p-4 ">
             <Card
               x-chunk="dashboard-02-chunk-0"
               className="border-green-400 border"
@@ -124,34 +121,24 @@ const DashboardLayout = () => {
               <nav className="grid gap-2 text-lg font-medium">
                 <Link
                   to={"/"}
-                  className="flex items-center gap-2 text-lg font-semibold"
+                  className="flex items-center gap-2 font-semibold"
                 >
-                  <Package2 className="h-6 w-6" />
-                  <span className="sr-only">Acme Inc</span>
+                  {/* <Package2 className="h-6 w-6" /> */}
+                  <img src={logo} alt="Life Book" width={120} />
                 </Link>
                 <Link
                   to={"/"}
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                 >
                   <Home className="h-5 w-5" />
-                  Dashboard
-                </Link>
-                <Link
-                  to={"/"}
-                  className="mx-[-0.65rem] flex items-center gap-4 rounded-xl bg-muted px-3 py-2 text-foreground hover:text-foreground"
-                >
-                  <ShoppingCart className="h-5 w-5" />
-                  Orders
-                  <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-                    6
-                  </Badge>
+                  Home
                 </Link>
                 <Link
                   to={"/"}
                   className="mx-[-0.65rem] flex items-center gap-4 rounded-xl px-3 py-2 text-muted-foreground hover:text-foreground"
                 >
                   <Package className="h-5 w-5" />
-                  Products
+                  Books
                 </Link>
               </nav>
               <div className="mt-auto">
@@ -183,26 +170,24 @@ const DashboardLayout = () => {
               </div>
             </form>
           </div>
+          <Button
+            variant="outline"
+            size="icon"
+            className="ml-auto h-8 w-8  hover:bg-green-100"
+          >
+            <Bell className="h-4 w-4" />
+            <span className="sr-only">Toggle notifications</span>
+          </Button>
           <DropdownMenu>
             <DropdownMenuTrigger asChild>
-              <div className="flex items-center gap-2">
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="ml-auto h-8 w-8  hover:bg-green-100"
-                >
-                  <Bell className="h-4 w-4" />
-                  <span className="sr-only">Toggle notifications</span>
-                </Button>
-                <Button
-                  variant="outline"
-                  size="icon"
-                  className="rounded-md h-8 w-8  hover:bg-green-100"
-                >
-                  <User2 className="h-4 w-4" />
-                  <span className="sr-only">Toggle user menu</span>
-                </Button>
-              </div>
+              <Button
+                variant="outline"
+                size="icon"
+                className="rounded-md h-8 w-8  hover:bg-green-100"
+              >
+                <User2 className="h-4 w-4" />
+                <span className="sr-only">Toggle user menu</span>
+              </Button>
             </DropdownMenuTrigger>
             <DropdownMenuContent align="end">
               <DropdownMenuLabel>My Account</DropdownMenuLabel>
